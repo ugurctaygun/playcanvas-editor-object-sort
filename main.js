9e4331a0951fa54e2ec504ece87b161ef99ca0ca
ugurctaygun <ugurctaygun@gmail.com>
2024-06-06 16:49:16 +0300

    sorting entities

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 // @namespace   Violentmonkey Scripts
 // @match       https://playcanvas.com/editor/scene/*
 // @grant       none
-// @version     1.0
+// @version     1.2
 // @author      -
 // @description 20/10/2021, 11:40:21
 // ==/UserScript==
@@ -15,34 +15,68 @@
 
     function sortEntityObject() {
         var selectedEntities = editor.selection.items;
-
+    
         if (selectedEntities.length === 0) {
             console.log("No entity selected");
             return;
         }
-
-        var entity = selectedEntities[0];
-
-        var children = entity.children;
-        var spacing = 1;
-        var currentPosition = new pc.Vec3(0, 0, 0);
-
-        editor.call('entities:history:startBatch', 'Sort Children'); 
-
-        for (const child of children) {
-            child.viewportEntity.setLocalPosition(currentPosition);
-            currentPosition.x += spacing;
-
-            editor.call('entities:insert', child); 
+    
+        var mapHolder = selectedEntities[0];
+        var children = mapHolder.children;
+    
+        var mapWidth = 10; 
+        var mapHeight = 10; 
+        var tileSize = 1; 
+    
+        if (children.length === 0) {
+            console.log("No children to sort");
+            return;
         }
-
-        editor.call('entities:history:endBatch'); 
-
+    
+        editor.call('entities:history:startBatch', 'Sort Children');
+    
+        var i = 0;
+        for (var y = 0; y < mapHeight; y++) {
+            for (var x = 0; x < mapWidth; x++) {
+                if (i >= children.length) {
+                    break;
+                }
+    
+                var offset = (y % 2 === 0) ? 0 : tileSize * 1;
+                var posX = x * tileSize * 2 + offset;
+                var posY = y * tileSize * Math.sqrt(3);
+                var pos = [posX, 0, posY];
+    
+                var child = children[i];
+                child.set('position', pos);
+    
+                editor.call('entities:insert', child);
+    
+                i++;
+            }
+            if (i >= children.length) {
+                break;
+            }
+        }
+    
+        editor.call('entities:history:endBatch');
+    
         console.log("Children sorted and changes saved");
+    
+        markSceneDirtyAndSave();
+    }
+
+    function markSceneDirtyAndSave() {
+        editor.call('scene:dirty');
+
+        setTimeout(() => {
+            editor.call('scene:save');
+            console.log("Scene saved");
+        }, 1000);
     }
 
     function createButton() {
-        const btn = new pcui.Button({ text: "Generate Boxes" });
+        const btn = new pcui.Button({ text: "Generate Hex Grid" });
         btn.style.position = "absolute";
         btn.style.bottom = "10px";
         btn.style.right = "10px";
