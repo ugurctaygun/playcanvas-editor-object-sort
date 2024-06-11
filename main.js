a635a907184a3b86e1a6b9221e0bb4a530bf67d8
ugurctaygun <ugurctaygun@gmail.com>
2024-06-11 22:24:40 +0300

    pcui popup

--- main.js
+++ main.js
@@ -9,83 +9,105 @@
 // ==/UserScript==
 
 (function () {
-    function init() {
-        createButton();
+  function init() {
+    createButton();
+    createPopup();
+  }
+
+  function sortEntityObject() {
+    var selectedEntities = editor.selection.items;
+
+    if (selectedEntities.length === 0) {
+      console.log("No entity selected");
+      return;
     }
 
-    function sortEntityObject() {
-        var selectedEntities = editor.selection.items;
-    
-        if (selectedEntities.length === 0) {
-            console.log("No entity selected");
-            return;
+    var mapHolder = selectedEntities[0];
+    var children = mapHolder.children;
+
+    var mapWidth = 10;
+    var mapHeight = 10;
+    var tileSize = 1;
+
+    if (children.length === 0) {
+      console.log("No children to sort");
+      return;
+    }
+
+    editor.call("entities:history:startBatch", "Sort Children");
+
+    var i = 0;
+    for (var y = 0; y < mapHeight; y++) {
+      for (var x = 0; x < mapWidth; x++) {
+        if (i >= children.length) {
+          break;
         }
-    
-        var mapHolder = selectedEntities[0];
-        var children = mapHolder.children;
-    
-        var mapWidth = 10; 
-        var mapHeight = 10; 
-        var tileSize = 1; 
-    
-        if (children.length === 0) {
-            console.log("No children to sort");
-            return;
-        }
-    
-        editor.call('entities:history:startBatch', 'Sort Children');
-    
-        var i = 0;
-        for (var y = 0; y < mapHeight; y++) {
-            for (var x = 0; x < mapWidth; x++) {
-                if (i >= children.length) {
-                    break;
-                }
-    
-                var offset = (y % 2 === 0) ? 0 : tileSize * 1;
-                var posX = x * tileSize * 2 + offset;
-                var posY = y * tileSize * Math.sqrt(3);
-                var pos = [posX, 0, posY];
-    
-                var child = children[i];
-                child.set('position', pos);
-    
-                editor.call('entities:insert', child);
-    
-                i++;
-            }
-            if (i >= children.length) {
-                break;
-            }
-        }
-    
-        editor.call('entities:history:endBatch');
-    
-        console.log("Children sorted and changes saved");
-    
-        markSceneDirtyAndSave();
+
+        var offset = y % 2 === 0 ? 0 : tileSize * 1;
+        var posX = x * tileSize * 2 + offset;
+        var posY = y * tileSize * Math.sqrt(3);
+        var pos = [posX, 0, posY];
+
+        var child = children[i];
+        child.set("position", pos);
+
+        editor.call("entities:insert", child);
+
+        i++;
+      }
+      if (i >= children.length) {
+        break;
+      }
     }
 
-    function markSceneDirtyAndSave() {
-        editor.call('scene:dirty');
+    editor.call("entities:history:endBatch");
 
-        setTimeout(() => {
-            editor.call('scene:save');
-            console.log("Scene saved");
-        }, 1000);
-    }
+    console.log("Children sorted and changes saved");
 
-    function createButton() {
-        const btn = new pcui.Button({ text: "Generate Hex Grid" });
-        btn.style.position = "absolute";
-        btn.style.bottom = "10px";
-        btn.style.right = "10px";
-        editor.call("layout.viewport").append(btn);
+    markSceneDirtyAndSave();
+  }
 
-        btn.on("click", () => {
-            sortEntityObject();
-        });
-    }
+  function markSceneDirtyAndSave() {
+    editor.call("scene:dirty");
 
-    editor.once("load", () => init());
+    setTimeout(() => {
+      editor.call("scene:save");
+      console.log("Scene saved");
+    }, 1000);
+  }
+
+  function createPopup() {
+    const popupOverlay = new pcui.Container();
+    const optionsElement = new pcui.SelectInput({
+      options: [
+        { t: "Hex", v: "Hex" },
+        { t: "Grid", v: "Grid" },
+      ],
+      defaultValue: [{ t: "Grid", v: "Grid" }],
+    });
+    popupOverlay.append(optionsElement);
+    popupOverlay.style.position = "absolute";
+    popupOverlay.style.background = "#364346";
+    popupOverlay.style.bottom = "30px";
+    popupOverlay.style.right = "10px";
+    popupOverlay.style.width = "150px";
+    editor.call("layout.viewport").append(popupOverlay);
+  }
+
+  function createButton() {
+    const btn = new pcui.Button({ text: "Generate Hex Grid" });
+    btn.style.position = "absolute";
+    btn.style.bottom = "10px";
+    btn.style.right = "10px";
+    editor.call("layout.viewport").append(btn);
+
+    btn.on("click", () => {
+      const popup = document.getElementById("sortPopup");
+      if (popup) {
+        popup.style.display = "block";
+      }
+    });
+  }
+
+  editor.once("load", () => init());
 })();
